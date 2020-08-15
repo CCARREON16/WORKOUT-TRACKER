21733645a552498759a4d1a32dc70416c15238b7
Chris Carreon <mrwiggles6545@yahoo.com>
2020-08-15 11:47:46 -0500

    changed workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,54 +1,54 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// const workoutSchema = new Schema ({
+const workoutSchema = new Schema ({
   
-//   day: {
-//     type: Date,
-//     default: () => new Date()
+  day: {
+    type: Date,
+    default: () => new Date()
 
-//   },
-//   excercises:  
-//   {
-//   type: {
-//     type: String,  
-//     trim: true,
-//     required: "enter excercise type"
-//   },
-//   name: {
-//       type: String,
-//       trim: true, 
-//       required: "enter a excercise name"
-//   },
-//   duration: {
-//     type: Number,
-//     trim: true, 
-//     required: "How many minutes was the duration of your workout?"
+  },
+  excercises:  
+  {
+  type: {
+    type: String,  
+    trim: true,
+    required: "enter excercise type"
+  },
+  name: {
+      type: String,
+      trim: true, 
+      required: "enter a excercise name"
+  },
+  duration: {
+    type: Number,
+    trim: true, 
+    required: "How many minutes was the duration of your workout?"
 
-// },
-//  weight: {
-//      type: Number,
+},
+ weight: {
+     type: Number,
      
-//  },
-//   reps: {
-//     type: Number,
+ },
+  reps: {
+    type: Number,
     
-// },
-// sets: {
-//     type: Number,
+},
+sets: {
+    type: Number,
     
-// },
-//  distance: {
-//     type: Number,
+},
+ distance: {
+    type: Number,
     
-// }
-//   }
-// });
+}
+  }
+});
 
-// const workout = mongoose.model("workout", workoutSchema);
+const workout = mongoose.model("workout", workoutSchema);
 
-// module.exports = workout;
+module.exports = workout;
 
 
 
